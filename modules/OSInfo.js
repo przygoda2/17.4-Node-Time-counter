d3f71151c57a6e66a7520d36ed61c0215b8f3ccd
Piotr Przygoda <przygoda@gmail.com>
2019-03-24 20:29:46 +0100

    Finished

---
+++ modules/OSInfo.js
@@ -0,0 +1,23 @@
+var os = require('os');
+
+function getOSinfo() {
+    var type = os.type();
+    if(type === 'Darwin') {
+        type = 'OSX';
+    } else if(type === 'Windows_NT') {
+        type = 'Windows';
+    }
+    var release = os.release();
+    var cpu = os.cpus()[0].model;
+    var uptime = os.uptime();
+    var userInfo = os.userInfo();
+    console.log('System:', type);
+    console.log('Release:', release);
+    console.log('CPU model:', cpu);
+    // console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
+    time.print();
+    console.log('User name:', userInfo.username);
+    console.log('Home dir:', userInfo.homedir);
+}
+
+exports.print = getOSinfo; // odpowiednik OSinfo.getOSinfo
