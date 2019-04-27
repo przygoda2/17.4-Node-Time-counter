cdc651bc4899577f46374010fa8dc834744eada1
Piotr Przygoda <przygoda@gmail.com>
2019-04-27 20:43:24 +0200

    Finished

--- modules/time.js
+++ modules/time.js
@@ -1,20 +1,35 @@
 var os = require('os');
 
-function time() {
-    const uptime = os.uptime();
-    var h = Math.floor(uptime / 3600);
-    var m = Math.floor(uptime % 3600 / 60);
-    var s = Math.floor(uptime % 3600 % 60);
+function time(uptime) {
 
+  var h = Math.floor(uptime / 3600);
+  var m = Math.floor(uptime % 3600 / 60);
+  var s = Math.floor(uptime % 3600 % 60);
+
+  var hours = h > 0 ? h : '';
+  var minutes = m > 0 ? m : '';
+  var seconds = s > 0 ? s : '';
+  
+  return {
+    hours,
+    minutes,
+    seconds,
+  };
+}
+
+exports.calculateFromSeconds = time;
+
+/* 2
+function time() {
     var hours = h > 0 ? h + (h === 1 ? " hour, " : " hours, ") : "";
     var minutes = m > 0 ? m + (m === 1 ? " minute, " : " minutes, ") : "";
     var seconds = s > 0 ? s + (s === 1 ? " second" : " seconds") : "";
     console.log(hours,minutes,seconds); 
 }
 exports.print = time;
+*/
 
-/*
-
+/* 1
 var os = require('os');
 var uptime = os.uptime();
 
@@ -34,5 +49,4 @@ function time() {
     }
 }
 exports.print = time;
-
 */
